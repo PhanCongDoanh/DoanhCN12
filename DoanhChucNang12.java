6e2de55a41b957878c27d9c4bf90bcd8a2f55b56
PhanCongDoanh <pcd20041807@gmail.com>
2025-05-16 09:48:57 +0700

    Doanh

---
+++ DoanhChucNang12.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appdevpracbai6;
+
+/**
+ *
+ * @author Admin
+ */
+public class DoanhChucNang12 {
+
+    public int cong(int a, int b){
+        return a + b;
+    }
+    
+    public float tinhTien(int soDau, int soCuoi){
+        return (soDau - soCuoi) * 1000;
+    }
+    
+}
